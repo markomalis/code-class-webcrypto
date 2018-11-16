91c98dce2da265c0ef0de729baeb0af4dfc5aa68
Marko Malis <markomalis@hotmail.com>
2018-11-16 07:15:33 +0100

    descriptions

--- src/client/lib/decrypt-message.js
+++ src/client/lib/decrypt-message.js
@@ -3,8 +3,19 @@ import {
   convertStringToArrayBufferView
 } from './buffer-conversion'
 
+/*
+  The default export should be a function that takes:
+    - An object containing:
+      - encrypted content of a message
+      - iv used to encrypt this content
+    - A privateKey as returned by the crypto.subtle.generateKey() method
 
 
-export default function({ content }) {
+  The function should use crypto.subtle.decrypt() to decrypt the message.
+  Then stringify the decrypted message and return it
+  (See 'spec' below)
+*/
+
+export default function({ content, iv }, privateKey) {
   return Promise.resolve(content)
 }
